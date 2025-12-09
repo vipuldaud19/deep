b5a0421cf868ae22a876d18c33b27d3cc9a890b6
Vipul Daud <vipuldaud19@gmail.com>
2025-12-09 11:02:59 +0530

    first commit

--- src/main/java/com/example/realestate/RealestateApplication.java
+++ src/main/java/com/example/realestate/RealestateApplication.java
@@ -2,15 +2,23 @@ package com.example.realestate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
 @EntityScan("com.example.realestate.model")
 @EnableJpaRepositories("com.example.realestate.repository")
-public class RealestateApplication {
+public class RealestateApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RealestateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RealestateApplication.class, args);
+    }
 
+    // ⭐ Required for WAR deployment
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RealestateApplication.class);
+    }
 }
