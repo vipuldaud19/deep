fc850872614d1c861e45a3f705fd37d26eb26c32
Vipul Daud <vipuldaud19@gmail.com>
2025-12-09 10:31:40 +0530

    first commit

--- src/main/java/com/example/realestate/controller/HomeController.java
+++ src/main/java/com/example/realestate/controller/HomeController.java
@@ -6,15 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    // Root URL -> index.jsp
     @GetMapping("/")
     public String home() {
-        return "index";
-    }
-
-    // Custom error page -> error.jsp
-    @GetMapping("/error-page")
-    public String errorPage() {
-        return "error";
+        return "index"; // This should load index.jsp
     }
 }
